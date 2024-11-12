1cc0aa351a2ee83f171204b8211c7d0f7a4935f3
tomasdornak <dornas444@seznam.cz>
2024-11-12 14:31:47 +0100

    12.11

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ document.addEventListener("DOMContentLoaded", function() {
         linksContainer.style.display = linksContainer.style.display === 'flex' ? 'none' : 'flex';
     });
 });
+
+
+
+const image = document.getElementById('zoom-image');
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  const images = document.querySelectorAll('.work-image');
+  
+  const modal = document.getElementById("myModal");
+  const modalImg = document.getElementById("img01");
+  const captionText = document.getElementById("caption");
+  
+  images.forEach((image) => {
+      image.addEventListener("click", function() {
+          modal.style.display = "block"; 
+          modalImg.src = this.src;  
+          captionText.innerHTML = this.alt;  
+      });
+  });
+
+  const closeBtn = document.getElementsByClassName("close")[0];
+  closeBtn.addEventListener("click", function() {
+      modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function(event) {
+      if (event.target === modal) {
+          modal.style.display = "none";
+      }
+  });
+});
